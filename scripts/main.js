520171215e363672689ba048078e3c0a6154218c
axelom <axelmed789@gmail.com>
2021-07-06 19:35:54 -0400

    create a personalized greeting

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,22 @@ myImage.onclick = function() {
       myImage.setAttribute('src','images/firefox-icon.png');
     }
 }
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+}
